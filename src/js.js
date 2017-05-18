60d25331c0451bba3e9fd76dadf107b9457b7656
jongro <jongro@users.noreply.github.com>
2017-05-18 22:31:08 +0200

    Update js.js

--- src/js.js
+++ src/js.js
@@ -6,16 +6,16 @@ $(window).on('load', function () {
     $("figure").fadeToggle(0);
 });
 
-$(document).ready(function () {
-    $.get('images/2017/2017.txt', function (data) {
-        var title1 = data.split("\n")[1].split("\t")[1];
-        var title2 = data.split("\n")[2].split("\t")[1];
-        var title3 = data.split("\n")[3].split("\t")[1];
-        $('#title1').html(title1);
-        $('#title2').html(title2);
-        $('#title3').html(title3);
-    });
-});
+//$(document).ready(function () {
+//    $.get('images/2017/2017.txt', function (data) {
+//        var title1 = data.split("\n")[1].split("\t")[1];
+//        var title2 = data.split("\n")[2].split("\t")[1];
+//        var title3 = data.split("\n")[3].split("\t")[1];
+//        $('#title1').html(title1);
+//        $('#title2').html(title2);
+//        $('#title3').html(title3);
+//    });
+//});
 
 $(document).ready(function () {
     $('#titlex1').load('images/2017/01.txt');
