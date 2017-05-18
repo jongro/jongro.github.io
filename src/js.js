00bb77114effbc01db3b3fd2aa459ca0ef55e44a
jongro <jongro@users.noreply.github.com>
2017-05-18 21:22:25 +0200

    Update js.js

--- src/js.js
+++ src/js.js
@@ -2,11 +2,9 @@
 
 $(document).ready(function () {
     $.get('images/2017/2017.txt', function (data) {
-        var title = data.split("\n")[0].split("\t")[1];
         var title1 = data.split("\n")[1].split("\t")[1];
         var title2 = data.split("\n")[2].split("\t")[1];
         var title3 = data.split("\n")[3].split("\t")[1];
-        $('#title').html(title);
         $('#title1').html(title1);
         $('#title2').html(title2);
         $('#title3').html(title3);
@@ -14,5 +12,7 @@ $(document).ready(function () {
 });
 
 $(document).ready(function () {
-    $('#here').load('images/2017/01.txt');
+    $('#titlex1').load('images/2017/01.txt');
+    $('#titlex2').load('images/2017/02.txt');
+    $('#titlex3').load('images/2017/03.txt');
 });
