7ccc11cb8794b3779900b0593b7fb8c60757185e
jongro <jongro@users.noreply.github.com>
2017-05-19 15:03:01 +0200

    Update js.js

--- src/js.js
+++ src/js.js
@@ -37,7 +37,7 @@ $(window).on('load', function () {
 
 
 $(document).ready(function () {
-    var k = 3;   
+    var k = 7;   
     var text = new Array(k);
     for (var i = 1; i <=k; i++)
         text[i-1] = '<figure style="display:block;"><img src="images/2017/0' + i + '.jpg" /><figcaption id="title' + i + '"></figcaption></figure>'
@@ -47,7 +47,7 @@ $(document).ready(function () {
     
 
  $(document).ready(function () {
-    var k = 3;     
+    var k = 7;     
     for (var i = 1; i <=k; i++)
         $('#title'+i).load('images/2017/0'+i+'.txt');
     
