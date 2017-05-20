2feb44d2039a0b71dccf75e52b6b904ef88704fc
jongro <jongro@users.noreply.github.com>
2017-05-20 16:45:57 +0200

    Update js.js

--- src/js.js
+++ src/js.js
@@ -6,23 +6,3 @@ $(window).on('load', function () {
     $("figure").css({ opacity: 1 });
 });
 
-
-//$(document).ready(function () {
-//    $.get('images/2017/2017.txt', function (data) {
-//        var title1 = data.split("\n")[1].split("\t")[1];
-//        var title2 = data.split("\n")[2].split("\t")[1];
-//        var title3 = data.split("\n")[3].split("\t")[1];
-//        $('#title1').html(title1);
-//        $('#title2').html(title2);
-//        $('#title3').html(title3);
-//    });
-//})
-
- $(document).ready(function () {
-    var k = 7;     
-    for (var i = 1; i <=k; i++)
-        $('#title'+i).load('images/2017/0'+i+'.txt');
-    
-    
-});
-
