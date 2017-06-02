7ffc2b2431a51d94f4f712bdb59949f412b7ec12
jongro <jongro@users.noreply.github.com>
2017-06-02 22:35:28 +0200

    neu

--- src/script.js
+++ src/script.js
@@ -1,32 +1,64 @@
 
 $(window).on('load', function () {
-    $('body').on('dragstart', function (event) { event.preventDefault(); });
-    $("#load").fadeToggle(0);
-    $("#front").css({ visibility: 'visible' });
-    $("#images").css({ visibility: 'visible' });
-    $("#buttonleft").css({ visibility: 'visible' });
-    $("#buttonright").css({ visibility: 'visible' });
-    setTimeout(function () {
-        $('#imagescontainer').css({ visibility: 'visible' });
-    }, 0);
+    // Inhalte anzeigen wenn geladen
+        $('body').on('dragstart', function (event) { event.preventDefault(); });
+        $("#load").fadeToggle(0);
+        $("#front").css({ visibility: 'visible' });
+        $("#images").css({ visibility: 'visible' });
+        $("#contact").css({ visibility: 'visible' });
+        $("#buttonleft").css({ visibility: 'visible' });
+        $("#buttonright").css({ visibility: 'visible' });
+        setTimeout(function () {
+            $('#imagescontainer').css({ visibility: 'visible' });
+        }, 0);
 });
 
-$(window).on('load resize', function () {
-    if ($(window).width() < 960) {                 // an Breite von body anpassen
-        $("#buttonright").css({ left: '885px' });
-        $("#buttonleft").css({ left: '210px' });
-    }
-    else {
-        if ($(window).width() > 1100) {                // an Breite von body anpassen
-            $("#buttonright").css({ left: $(window).width() / 2 + 475 + 'px' });
-            $("#buttonleft").css({ left: $(window).width() / 2 - 340 + 'px' });
-        }
-        else {
-            $("#buttonright").css({ left: 'auto' });
-            $("#buttonleft").css({ left: '210px' });
 
-        }
-    }
+
+$(window).on('load resize', function () {
+        // Anpassen der Breite von #images -> zentriert
+        var width = 0;
+        $('#images figure').filter(function () { return $(this).css('display') !== 'none'; }).each(function () {
+            width += $(this).outerWidth(true) + 4;
+            $('#images').css('width', width + 38 + 'px');
+        });
+        // buttonright bleibt bei Fenstergröße kleiner body stehen
+        if ($(window).width() < 960) {                 // an Breite von body anpassen
+            $("#buttonright").css({ left: '905px' });
+        } else {
+            $("#buttonright").css({ left: 'auto' });
+        };
+});
+
+$(document).ready(function () {
+    // Anpassen der Breite von #images -> zentriert (IE)
+    var width = 0;
+    $('#images figure').filter(function () { return $(this).css('display') !== 'none'; }).each(function () {
+        width += $(this).outerWidth(true) + 4;
+        $('#images').css('width', width + 38 + 'px');
+
+    });
+        // buttonright bleibt bei Fenstergröße kleiner body stehen
+    if ($(window).width() < 960) {                 // an Breite von body anpassen
+        $("#buttonright").css({ left: '905px' });
+    } else {
+        $("#buttonright").css({ left: 'auto' });
+    };
+});
+
+
+$(window).on('load', function () {
+    // Bildunterschriften werden bei Touchgeräten immer angezeigt
+    try {
+        document.createEvent('TouchEvent');
+        alwShoNav = true;
+    } catch (e) { }
+
+    if (alwShoNav) {
+        $("figcaption").css({ visibility: 'visible' });
+        $("figcaption").css({ opacity: '1' });
+    };
+
 });
 
 
