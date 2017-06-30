f720285c4b7cc07633aa202ea90d8751087de958
jongro <jongro@users.noreply.github.com>
2017-06-30 11:56:31 +0200

    neu

--- src/script.js
+++ src/script.js
@@ -12,7 +12,7 @@ $(window).on('load', function () {
     }, 0);
 });
 
-$(window).on('load resize', function () {
+$(window).on('load resize on', function () {
     // Anpassen der Breite von #images -> zentriert
     var width = 0;
     $('#images figure').filter(function () { return $(this).css('display') !== 'none'; }).each(function () {
