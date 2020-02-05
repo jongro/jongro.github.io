0677ffe1c65a322e734c9dee36f666dbf42aa140
jongro <jonas.groening@t-online.de>
2020-02-05 22:37:25 +0100

    new

---
+++ src/js/functions.js
@@ -0,0 +1,34 @@
+window.onload = function () {
+	document.getElementById("year").innerHTML = new Date().getFullYear();
+}
+
+new AnimOnScroll( document.getElementById( 'grid' ), {
+	minDuration : 0.4,
+	maxDuration : 0.7,
+	viewportFactor : 0.2
+} );
+
+function toggle() {
+	document.getElementById("button-menu").classList.toggle("is-active");
+	var nav = document.querySelector("nav ul");
+	if (nav.style.visibility === "visible") {
+		nav.style.visibility = "hidden";
+		nav.style.opacity = "0"
+		nav.style.transition = "opacity 0 ease"
+	} else {
+		nav.style.visibility = "visible";
+		nav.style.opacity = "0.95"
+		nav.style.transition = "opacity 0.5s ease"
+
+	}
+}
+
+
+var lightbox = GLightbox({
+	loop: true
+});
+var lightboxDescription = GLightbox({
+	selector: 'glightbox-description',
+});
+
+
