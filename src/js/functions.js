3e7c41d935e75b2209d3cf2dcc5a415e07e7e983
jongro <jonas.groening@t-online.de>
2021-01-30 15:32:29 +0100

    Update functions.js

--- src/js/functions.js
+++ src/js/functions.js
@@ -9,5 +9,5 @@ let imgs = document.getElementsByTagName("img");
 for (i = 0; i < imgs.length; i++) { 
 
     imgs[i].style.marginTop = String(Math.random() * 50) + "%"; 
-    imgs[i].style.marginLeft = String((Math.random() -0.5) * 50) + "%";       
+    imgs[i].style.marginLeft = String((Math.random() -0.5) * 25) + "%";       
 }
