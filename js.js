19df6ae8b210b9848965131b5b6593d13965c4ef
jongro <jonas.groening@t-online.de>
2024-02-28 15:37:19 +0100

    Update js.js

--- js.js
+++ js.js
@@ -5,8 +5,7 @@ text = document.getElementsByTagName("p")[0]
 text.addEventListener("click", fadeout);
 
 function fadeout() {
-    text.style.transform = "scale(0)"
+    text.style.transform = "scale(0)";
     setTimeout(function(){text.style.opacity = "0";}, 0.3);
-
-    document.title = '\u00A0';
+    setTimeout(function(){document.title = '\u00A0';}, 0.3);
 }
