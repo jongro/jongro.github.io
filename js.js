1ffdc4d5a2da09cf1c095d50fe459cda89a55681
jongro <jonas.groening@t-online.de>
2024-02-28 15:32:24 +0100

    Update js.js

--- js.js
+++ js.js
@@ -8,5 +8,5 @@ function fadeout() {
     text.style.transform = "scale(0)"
     setTimeout(function(){text.style.opacity = "0";}, 0.3);
 
-    document.title = "";
+    document.title = '\u00A0';
 }
