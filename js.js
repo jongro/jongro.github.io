ed65687d89b5080f166405b4301de43a1eb2b074
jongro <jonas.groening@t-online.de>
2024-07-25 22:53:56 +0200

    Update js.js

--- js.js
+++ js.js
@@ -7,13 +7,13 @@ text.addEventListener("click", fadeout);
 function fadeout() {
     text.style.transform = "scale(0)";
     text.style.opacity = "0";
-    setTimeout(function(){document.title = '\u200B;';}, 750);
-    setTimeout(function(){document.title = '\u200C;';}, 1750);
-    setTimeout(function(){document.title = '\u200D;';}, 2750);
-    setTimeout(function(){document.title = '\u200E;';}, 3750);
-    setTimeout(function(){document.title = '\u2060;';}, 4750);
-    setTimeout(function(){document.title = '\u206D;';}, 5750);
-    setTimeout(function(){document.title = '\u206F;';}, 6750);
+    setTimeout(function(){document.title = '\u200B';}, 750);
+    setTimeout(function(){document.title = '\u200C';}, 1750);
+    setTimeout(function(){document.title = '\u200D';}, 2750);
+    setTimeout(function(){document.title = '\u200E';}, 3750);
+    setTimeout(function(){document.title = '\u2060';}, 4750);
+    setTimeout(function(){document.title = '\u206D';}, 5750);
+    setTimeout(function(){document.title = '\u206F';}, 6750);
 
 
     //setTimeout(function(){document.open();}, 800);
